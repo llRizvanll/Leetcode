a2d12dc7394cbd36c22bde66cae1300913fe2505
llRizvanll <torizvan@gmail.com>
2020-01-05 13:10:53 +0530

    problem description added

--- src/leetjava/Anagram.java
+++ src/leetjava/Anagram.java
@@ -1,5 +1,24 @@
 package leetjava;
 
+/*
+
+valid anagram
+
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+Note:
+You may assume the string contains only lowercase alphabets.
+
+https://leetcode.com/problems/valid-anagram/
+ */
 public class Anagram {
 
     public static boolean isAnagram(String s, String t) {
